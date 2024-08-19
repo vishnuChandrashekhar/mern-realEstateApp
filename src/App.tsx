d29cda975b8968fc659720a0930751cdcd68b30b
vishnuChandrashekhar <154775783+vishnuChandrashekhar@users.noreply.github.com>
2024-08-19 06:08:28 +0530

    create pages and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Profile from './pages/profile'
+import Signin from './pages/signin'
+import About from './pages/about'
+import Signup from './pages/Signup'
+
+
 
 const App: React.FC = () => {
   
   return (
-    <div>
-      Hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Signin.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Signin: React.FC = () => {
+
+  return (
+    <div>signin</div>
+  )
+}
+
+export default Signin
