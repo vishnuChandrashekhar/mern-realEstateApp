45ebc3e4b801aedd215c78658f28574d96f27e3a
vishnuChandrashekhar <154775783+vishnuChandrashekhar@users.noreply.github.com>
2024-08-20 06:21:18 +0530

    Create adn Run the server

---
+++ API/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application,} from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app: Application = express()
+const PORT: string | number = process.env.PORT || 3000
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on : http://localhost:${PORT}`)
+})
