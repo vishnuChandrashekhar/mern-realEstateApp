940c1d05e013298c4c70f095460b47d17b91335a
vishnuChandrashekhar <154775783+vishnuChandrashekhar@users.noreply.github.com>
2024-08-20 19:29:27 +0530

    Connect to database

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,7 +1,20 @@
 import express, { Application,} from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+
 
 dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO||'')
+  .then(() => {
+  console.log(`Connected to the MongoDB`)
+})
+  .catch((err) => {
+  console.error(err);
+})
+
 const app: Application = express()
 const PORT: string | number = process.env.PORT || 3000
 
