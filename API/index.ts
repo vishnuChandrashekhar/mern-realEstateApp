71e528189aa004b019b6b491d14fa12ec7319d7b
vishnuChandrashekhar <154775783+vishnuChandrashekhar@users.noreply.github.com>
2024-10-27 22:25:53 +0530

    add pre installation

--- API/index.ts
+++ API/index.ts
@@ -1,12 +1,13 @@
 import dotenv from "dotenv";
 
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import mongoose from "mongoose";
 import userRouter from "./src/Routes/user.routes";
 import authRouter from "./src/Routes/auth.route";
 import { errorHandler } from "./src/utils/error.handler";
 import cookieParser from "cookie-parser";
 import listingRouter from "./src/Routes/listing.route";
+import path from "path";
 
 dotenv.config({ path: "./.env" });
 
@@ -23,6 +24,7 @@ mongoose
     console.error(err);
   });
 
+const __dirname = path.resolve();
 const app: Application = express();
 const PORT: string = process.env.PORT as string;
 
@@ -32,6 +34,11 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(errorHandler);
 
